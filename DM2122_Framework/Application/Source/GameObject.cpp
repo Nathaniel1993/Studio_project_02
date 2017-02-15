4da6e5b4f562d3361e063efbfba4960117f29f0c
lucifer967 <lucifer967@hotmail.com>
2017-02-15 10:22:44 +0800

    Changed GameObject Variables for (Experiemental) Collision

--- DM2122_Framework/Application/Source/GameObject.cpp
+++ DM2122_Framework/Application/Source/GameObject.cpp
@@ -2,10 +2,41 @@
 
 GameObject::GameObject()
 {
+	position_ = Vector3(0, 0, 0);
+	SizeX = 0;
+	SizeY = 0;
+}
 
+GameObject::GameObject(Vector3 newposition)
+{
+	position_ = newposition;
+	SizeX = 0;
+	SizeY = 0;
+}
+
+GameObject::GameObject(Vector3 newposition, int newsizeX, int newsizeY)
+{
+	position_ = newposition;
+	SizeX = newsizeX;
+	SizeY = newsizeY;
 }
 
 GameObject::~GameObject()
+{
+
+}
+
+Vector3 GameObject::getPosition()
+{
+	return position_;
+}
+
+int GameObject::getSizeX()
+{
+
+}
+
+int GameObject::getSizeY()
 {
 
 }
--- DM2122_Framework/Application/Source/GameObject.h
+++ DM2122_Framework/Application/Source/GameObject.h
@@ -1,17 +1,18 @@
-#ifndef GAME_OBJECT_H
-#define GAME_OBJECT_H
-
 #include "Vector3.h"
 
 class GameObject
 {
 public:
-	//Constructor & Destructor
 	GameObject();
+	GameObject(Vector3 newposition);
+	GameObject(Vector3 newposition, int newsizeX, int newsizeY);
 	~GameObject();
+	Vector3 getPosition();
+	int getSizeX();
+	int getSizeY();
 
 protected:
 	Vector3 position_;
+	int SizeX,
+		SizeY;
 };
-
-#endif // !GAME_OBJECT_H
